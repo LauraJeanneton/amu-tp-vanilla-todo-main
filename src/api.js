7681ea44a1d915b723acbcc834e077a31684c219
LauraJeanneton <laura.jeanneton@orange.fr>
2022-01-10 15:57:55 +0000

    Fin phase refactor

---
+++ src/api.js
@@ -0,0 +1,48 @@
+// src/api.js
+
+const SUPABASE_URL = "https://qirzvtdxtvskcoymxwvx.supabase.co/rest/v1/todos";
+const SUPABASE_API_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTY0MTgyNjkwNywiZXhwIjoxOTU3NDAyOTA3fQ.leSP7foKa6Ig5rHVJ9dcac5UD9JVcanklfFWTRIbXIY";
+
+/**
+ * Récupère les items sur Supabase
+ * @returns Promise<array>
+ */
+export const loadTodoItemsFromApi = () => {
+  return fetch(`${SUPABASE_URL}?order=created_at`, {
+    headers: {
+      apiKey: SUPABASE_API_KEY,
+    },
+  }).then((response) => response.json());
+};
+
+/**
+ * Modifie le statut d'une tâche sur Supabase
+ * @returns Promise<array>
+ */
+export const toggleComplete = (id, done) => {
+  return fetch(`${SUPABASE_URL}?id=eq.${id}`, {
+    method: "PATCH",
+    headers: {
+      "Content-Type": "application/json",
+      apiKey: SUPABASE_API_KEY,
+      Prefer: "return=representation",
+    },
+    body: JSON.stringify({ done: done }),
+  });
+};
+
+/**
+ * Créé une nouvelle tâche dans Supabase
+ * @returns Promise<{id: number, text: string, done: boolean}>
+ */
+export const saveTodoItemToApi = (item) => {
+  return fetch(SUPABASE_URL, {
+    method: "POST",
+    body: JSON.stringify(item),
+    headers: {
+      "Content-Type": "application/json",
+      apiKey: SUPABASE_API_KEY,
+      Prefer: "return=representation",
+    },
+  }).then((response) => response.json());
+};
--- src/app.js
+++ src/app.js
@@ -1,51 +1,101 @@
 // src/app.js
 
-// On créé ici un tableau TODO_ITEMS qui contient deux objets 
-const TODO_ITEMS = [
-    { id: 1, text: "Faire les courses", done: false },
-    { id: 2, text: "Aller chercher les enfants", done: true },
-  ];
-  
-  // Nous créons une fonction qui servira à ajouter un élément dans le UL à partir d'un objet tâche
-  const addTodo = (item) => {
-      // On sélectionne le <ul>
-    const container = document.querySelector("ul");
-  
-    // On ajoute du HTML à la fin du <ul>
-    container.insertAdjacentHTML(
-      "beforeend",
-      `
-          <li>
-              <label>
-                  <input type="checkbox" id="todo-${item.id}" ${item.done ? "checked" : ""} /> 
-                  ${item.text}
-              </label>
-          </li>
-      `
-    );
+// On importe dans notre module principal des fonctions en provenance
+// du module api.js
+import {
+  loadTodoItemsFromApi,
+  toggleComplete,
+  saveTodoItemToApi,
+} from "./api.js";
+
+const SUPABASE_URL = "https://qirzvtdxtvskcoymxwvx.supabase.co/rest/v1/todos";
+const SUPABASE_API_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTY0MTgyNjkwNywiZXhwIjoxOTU3NDAyOTA3fQ.leSP7foKa6Ig5rHVJ9dcac5UD9JVcanklfFWTRIbXIY";
+
+// src/app.js
+
+// Dès le chargement des élements du DOM
+document.addEventListener("DOMContentLoaded", () => {
+  // On appelle l'API pour récupérer les tâches
+  loadTodoItemsFromApi().then((items) => {
+    // Pour chaque tâche, on l'affiche dans l'interface
+    items.forEach((item) => addTodo(item));
+  });
+});
+
+/**
+ * Gestion du click sur une Checkbox
+ * @param {MouseEvent} e
+ */
+const onClickCheckbox = (e) => {
+  // On récupère l'identifiant de la tâche cliquée (todo-1 ou todo-12 par exemple)
+  const inputId = e.target.id;
+  // On ne garde que la partie numérique (1 ou 12 par exemple)
+  const id = +inputId.split("-").pop();
+  // On récupère le fait que la checkbox soit cochée ou pas lors du click
+  const isDone = e.target.checked;
+
+  // On annule le comportement par défaut de l'événement (cocher ou décocher la case)
+  // Car on ne souhaite cocher / décocher que si le traitement va au bout
+  e.preventDefault();
+
+  // On appelle l'API afin de changer le statut de la tâche
+  toggleComplete(id, isDone).then(() => {
+    // Lorsque c'est terminé, on coche ou décoche la case
+    e.target.checked = isDone;
+  });
+};
+
+/**
+ * Permet d'ajouter visuellement une tâche dans l'interface
+ * @param {{id: number, text: string, done: boolean}} item
+ */
+const addTodo = (item) => {
+  // On récupère le <ul>
+  const container = document.querySelector("ul");
+
+  // On intègre le HTML de la tâche à la fin du <ul>
+  container.insertAdjacentHTML(
+    "beforeend",
+    `
+        <li>
+            <label>
+                <input type="checkbox" id="todo-${item.id}" ${item.done ? "checked" : ""} /> 
+                ${item.text}
+            </label>
+        </li>
+    `
+  );
+
+  // Alors que la tâche a été ajoutée, on attache au click sur la checkbox la fonction onClickCheckbox
+  document
+    .querySelector("input#todo-" + item.id)
+    .addEventListener("click", onClickCheckbox);
+};
+
+// Gestion de la soumission du formulaire
+document.querySelector("form").addEventListener("submit", (e) => {
+  // On annule le comportement par défaut de la soumission 
+  // (qui aurait pour effet de recharger la page, ce qu'on ne souhaite pas vu qu'on souhaite gérer nous-même le comportement)
+  e.preventDefault();
+
+  // On récupère l' <input /> du formulaire
+  const input = document.querySelector('input[name="todo-text"]');
+
+  // On créé une tâche avec la valeur de l'<input />
+  const item = {
+    text: input.value,
+    done: false,
   };
-  document.querySelector("form").addEventListener("submit", (event) => {
-    // On souhaite aussi empêcher le rechargement de la page
-    event.preventDefault();
   
-    // On récupère l'input
-    const input = document.querySelector('input[name="todo-text"]');
-  
-    // On créé une nouvelle tâche avec pour text la valeur tapée dans l'input
-    const item = {
-      id: Date.now(),
-      text: input.value,
-      done: false,
-    };console.log(input.value);
-    
-    // On appelle la fonction créée plus tôt qui ajoutera la tâche dans le <ul>
-    addTodo(item);
-  
-    // On vide l'input et replace le curseur dedans
+  // On appelle l'API afin de sauver la nouvelle tâche
+  saveTodoItemToApi(item).then((items) => {
+    // La réponse de l'API est un tableau avec les tâches
+    // touchées par le traitement, on prend la première (la seule en fait)
+    // Et on l'affiche dans l'interface
+    addTodo(items[0]);
+
+    // On vide l'<input /> et on remet le curseur dessus
     input.value = "";
     input.focus();
   });
-  // Pour chaque élément du tableau TODO_ITEMS, on appelle la fonction addTodo en fournissant
-  // l'item
-  TODO_ITEMS.forEach((item) => addTodo(item));// On souhaite réagir à chaque fois que le formulaire est soumis
- 
+});
