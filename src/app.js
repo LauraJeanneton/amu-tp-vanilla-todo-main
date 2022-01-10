675dcb507422e194d1203128d610c90a07d8754b
LauraJeanneton <laura.jeanneton@orange.fr>
2022-01-10 14:43:14 +0000

    Add list

--- src/app.js
+++ src/app.js
@@ -1,1 +1,51 @@
-console.log("Tout fonctionne");
+// src/app.js
+
+// On créé ici un tableau TODO_ITEMS qui contient deux objets 
+const TODO_ITEMS = [
+    { id: 1, text: "Faire les courses", done: false },
+    { id: 2, text: "Aller chercher les enfants", done: true },
+  ];
+  
+  // Nous créons une fonction qui servira à ajouter un élément dans le UL à partir d'un objet tâche
+  const addTodo = (item) => {
+      // On sélectionne le <ul>
+    const container = document.querySelector("ul");
+  
+    // On ajoute du HTML à la fin du <ul>
+    container.insertAdjacentHTML(
+      "beforeend",
+      `
+          <li>
+              <label>
+                  <input type="checkbox" id="todo-${item.id}" ${item.done ? "checked" : ""} /> 
+                  ${item.text}
+              </label>
+          </li>
+      `
+    );
+  };
+  document.querySelector("form").addEventListener("submit", (event) => {
+    // On souhaite aussi empêcher le rechargement de la page
+    event.preventDefault();
+  
+    // On récupère l'input
+    const input = document.querySelector('input[name="todo-text"]');
+  
+    // On créé une nouvelle tâche avec pour text la valeur tapée dans l'input
+    const item = {
+      id: Date.now(),
+      text: input.value,
+      done: false,
+    };console.log(input.value);
+    
+    // On appelle la fonction créée plus tôt qui ajoutera la tâche dans le <ul>
+    addTodo(item);
+  
+    // On vide l'input et replace le curseur dedans
+    input.value = "";
+    input.focus();
+  });
+  // Pour chaque élément du tableau TODO_ITEMS, on appelle la fonction addTodo en fournissant
+  // l'item
+  TODO_ITEMS.forEach((item) => addTodo(item));// On souhaite réagir à chaque fois que le formulaire est soumis
+ 
